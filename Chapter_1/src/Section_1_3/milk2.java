f04d63aba40e3b1e04b830b9c7dc7af95320cfac
Chuyeon Lee <wonup2@hotmail.com>
2019-06-04 19:22:04 -0700

    test

---
+++ Chapter_1/src/Section_1_3/milk2.java
@@ -0,0 +1,10 @@
+package Section_1_3;
+
+public class milk2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter_1/src/template.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class template {
+
+	static Scanner in;
+	static PrintWriter out;
+	static String INFILE="template.in";
+	static String OUTFILE="template.out";
+
+	
+	public static void main(String[] args) {
+		try {
+			
+			init();
+			solve();
+			
+		
+			
+		
+		} 
+		catch(Exception e) {
+			System.out.println(e);
+		}		
+	}
+	
+	static void init(){
+		
+	}
+	
+	static String solve() {
+		return null;
+	}
+
+}
