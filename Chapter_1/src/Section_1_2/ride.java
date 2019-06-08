511aa00603c9fc0c1925a377fe86ed945812eb60
Chuyeon Lee <wonup2@hotmail.com>
2019-06-08 14:38:12 -0700

    ride

--- Chapter_1/src/Section_1_2/ride.java
+++ Chapter_1/src/Section_1_2/ride.java
@@ -6,15 +6,24 @@ public class ride {
 
 	static Scanner in;
 	static PrintWriter out;
+	static String input[];
+	
+	static String result; 
 	
 	public static void main(String[] args) {
 
 		try {
 			
-			init();
-			solve();
+			in = new Scanner(new File("ride.in"));
+			out = new PrintWriter(new File("ride.out"));
 			
-		
+			init();
+			result = solve();
+			
+			out.println(result);
+			
+			in.close();
+			out.close();
 		
 		} 
 		catch(Exception e) {
@@ -24,11 +33,27 @@ public class ride {
 	
 
 	static void init(){
-		
+		input = new String[2];
+		input[0] = in.nextLine();
+		input[1] = in.nextLine();
 	}
 	
 	static String solve() {
-		return null;
+		
+		//step 1 - convert input string to integer
+		int n1 = convert(input[0]);
+		int n2 = convert(input[1]);
+		
+		//step 2 - compare first input with input
+		if(n1==n2) return "GO";
+		else return "STAY";		
 	}
-
+	
+	static int convert(String s) {
+		int n = 1;
+		for(int i=0; i<s.length(); i++) {
+			n*=(s.charAt(i)-'A'+1);
+		}
+		return n%47;
+	}
 }
