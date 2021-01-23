8bf43740ca063b11fc769981ebedd6395f994d34
Chuyeon Lee <wonup2@hotmail.com>
2021-01-23 05:09:10 -0800

    .

--- Contest_2017_2018/src/US_SIL_lemonade.java
+++ Contest_2017_2018/src/US_SIL_lemonade.java
@@ -1,33 +1,41 @@
-//US Open Contest, Silver
-//lemonade
 import java.util.*;
 import java.io.*;
 
 public class US_SIL_lemonade {
-
+	
+	static BufferedReader in;
+	static PrintWriter out;
+	static int a[], n;
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new FileReader("lemonade.in"));
-		PrintWriter out = new PrintWriter(new FileWriter("lemonade.out"));
+		in = new BufferedReader(new FileReader("lemonade.in"));
+		out = new PrintWriter(new BufferedWriter(new FileWriter("lemonade.out")));	
+		init();
+		solve();
+		in.close();
+		out.close();
+	}		
+	
+	static void init() throws NumberFormatException, IOException {
 		
-		int n = Integer.parseInt(in.readLine());
-		int[] a = new int[n];
+		n = Integer.parseInt(in.readLine());
+		a = new int[n];
+		
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		for(int i=0; i<n; i++){
+		for(int i=0; i<n; i++)
 			a[i] = Integer.parseInt(st.nextToken());
-		}
-		
+				
 		Arrays.sort(a);
-		//System.out.println(Arrays.toString(a));
+	}
+	
+	static void solve() {
 		
 		int ans=0;
 		for(int i=n-1; i>=0; i--){
 			if(a[i]<ans) break;
 			ans++;
-			//System.out.print(ans+" ");
 		}
 		
 		out.println(ans);
-		in.close();
-		out.close();
 	}
 }
