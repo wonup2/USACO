49318aa4c1d1fa803e31140a9a7dff1a47f8abde
Chuyeon Lee <wonup2@hotmail.com>
2021-01-23 05:21:19 -0800

    ,

--- Contest_2019_2020/src/DEC_SIL_moobuzz.java
+++ Contest_2019_2020/src/DEC_SIL_moobuzz.java
@@ -7,18 +7,17 @@ public class DEC_SIL_moobuzz {
 		Scanner in = new Scanner(new File("moobuzz.in"));
 		PrintWriter out = new PrintWriter(new File("moobuzz.out"));
 		
-		int[] a = {0, 1, 2, 4, 7, 8, 11, 13};
+		int[] a = {14, 1, 2, 4, 7, 8, 11, 13};
 		int n = in.nextInt();
 
 		int ans = 0;
 	    int mult = n / 8;
 	    int mod = n % 8;
 	    
-	    if(mod==0) mult--;
+	    if(mod==0) mult--;	  
 	    
 	    ans = (mult * 15) + a[mod];
 	    out.println(ans);
-	    
 		out.close();
 		in.close();
 	}
