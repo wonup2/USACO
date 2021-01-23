aab8196e8b3568f48c9ce88b04f6c92f212768a2
Chuyeon Lee <wonup2@hotmail.com>
2021-01-23 05:29:23 -0800

    .

--- Contest_2018_2019/src/FEB_BR_herding.java
+++ Contest_2018_2019/src/FEB_BR_herding.java
@@ -8,19 +8,15 @@ public class FEB_BR_herding {
 	static int[] a;
 	
 	public static void main(String[] args) throws IOException {
-		try {
-			init();
-			solve();			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}	
+		in = new Scanner(new File("herding.in"));
+		out = new PrintWriter(new File("herding.out"));
+		init();
+		solve();					
 		in.close();
 		out.close();
 	}
 	
-	static void init() throws IOException {
-		in = new Scanner(new File("herding.in"));
-		out = new PrintWriter(new File("herding.out"));
+	static void init() throws IOException {		
 		
 		a=new int[3];
 		a[0]=in.nextInt();
@@ -31,10 +27,13 @@ public class FEB_BR_herding {
 	}
 	
 	static void solve() throws IOException {
-		int r1=0, r2=0;
-		if(a[2]==a[0]+2) out.println("0");
-		else if(a[1]==a[0]+2||a[2]==a[1]+2) out.println("1");
-		else out.println("2");
+		
+		if(a[2] == a[0]+2) out.println(0);
+		
+		else if(a[1] == a[0]+2 || a[2] == a[1]+2) out.println(1);
+		
+		else out.println(2);
+		
 		out.println(Math.max(a[1]-a[0], a[2]-a[1])-1);
 	}	
 }
