d2b10cf9b395ead52f5889f21b4c86599628e7be
Chuyeon Lee <wonup2@hotmail.com>
2021-01-23 18:50:22 -0800

    Delete Students directory

--- Students/src/MyProgram.java
+++
@@ -1,6 +0,0 @@
-public class  MyProgram {
-    public static void main(String[] args) {
-        System.out.println("This is a test of the");
-        System.out.println("emergency broadcast system.");
-    }
-}
